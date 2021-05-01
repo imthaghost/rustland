8c6745663137f49ec6e93aa19388cae9310fa41c
imthaghost <imthaghost@protonmail.com>
2021-05-01 16:16:46 -0700

    [update] cleanup

---
+++ helloworld/target/debug/deps/helloworld.d
@@ -0,0 +1,5 @@
+/Users/ghost/rust/rustland/helloworld/target/debug/deps/helloworld: src/main.rs
+
+/Users/ghost/rust/rustland/helloworld/target/debug/deps/helloworld.d: src/main.rs
+
+src/main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/Users/ghost/rust/rustland/helloworld/target/debug/helloworld: /Users/ghost/rust/rustland/helloworld/src/main.rs
