053e2ec6ea0b3b6a54a17910f0c13a4128cd93f3
imthaghost <imthaghost@protonmail.com>
2021-05-01 16:22:10 -0700

    [update]

--- helloworld/target/debug/deps/helloworld.d
+++
@@ -1,5 +0,0 @@
-/Users/ghost/rust/rustland/helloworld/target/debug/deps/helloworld: src/main.rs
-
-/Users/ghost/rust/rustland/helloworld/target/debug/deps/helloworld.d: src/main.rs
-
-src/main.rs:
--- helloworld/target/debug/helloworld.d
+++
@@ -1,1 +0,0 @@
-/Users/ghost/rust/rustland/helloworld/target/debug/helloworld: /Users/ghost/rust/rustland/helloworld/src/main.rs
